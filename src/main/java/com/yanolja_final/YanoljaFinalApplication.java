7192da6371e0da4d6537459f9f19de11b293b38d
hym <ghrltjdtprbs@gmail.com>
2023-12-06 10:38:17 +0900

    [feat] 프로젝트 생성

---
+++ src/main/java/com/yanolja_final/YanoljaFinalApplication.java
@@ -0,0 +1,13 @@
+package com.yanolja_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanoljaFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YanoljaFinalApplication.class, args);
+    }
+
+}
