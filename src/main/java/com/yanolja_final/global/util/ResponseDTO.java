2de65cbdc16a1cf22c8e8239d80327027f6f44e1
hym <ghrltjdtprbs@gmail.com>
2023-12-06 10:44:36 +0900

    [feat] API 응답 통일을 위한 ResponseDTO 추가
    변경 사항 :
     - 애플리케이션 전반에 걸친 API 응답 구조를 표준화하기 위해 ResponseDTO 구현

---
+++ src/main/java/com/yanolja_final/global/util/ResponseDTO.java
@@ -0,0 +1,44 @@
+package com.yanolja_final.global.util;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ResponseDTO<T> {
+
+    private final int code;
+    private final String message;
+    private final T data;
+
+    @Builder
+    private ResponseDTO(int code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static ResponseDTO<Object> res(final HttpStatus httpStatus, final String message) {
+        return ResponseDTO.<Object>builder()
+            .code(httpStatus.value())
+            .message(message)
+            .build();
+    }
+
+    public static <T> ResponseDTO<T> res(final HttpStatus httpStatus, final T data) {
+        return ResponseDTO.<T>builder()
+            .code(httpStatus.value())
+            .data(data)
+            .build();
+    }
+
+    public static <T> ResponseDTO<T> res(final HttpStatus httpStatus,
+        final String message,
+        final T data) {
+        return ResponseDTO.<T>builder()
+            .code(httpStatus.value())
+            .message(message)
+            .data(data)
+            .build();
+    }
+}
