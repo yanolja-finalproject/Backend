898264e2168ef2f45d659983adfe40b74e4b6d45
Jinhong Kim <deepred.kim@gmail.com>
2023-12-19 16:07:42 +0900

    [refactor] Security Config 클래스 분리

---
+++ src/main/java/com/yanolja_final/global/config/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.yanolja_final.global.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // JWT 인증을 사용하므로 CSRF 보안 해제 및 세션 사용안함 설정
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(sessionManagement -> sessionManagement
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+        ;
+
+        http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+            .anyRequest().permitAll()
+        );
+
+        return http.build();
+    }
+}
