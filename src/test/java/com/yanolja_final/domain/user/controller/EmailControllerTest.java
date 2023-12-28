1c72ecd219bb2cce9cb7ac4bcb014af6b488eb43
hym <ghrltjdtprbs@gmail.com>
2023-12-28 18:35:41 +0900

    [feat] EmailControllerTest 추가
    변경 사항 :
     - rest docs 생성을 위한 EmailControllerTest 테스트 코드를 작성하였습니다.
     - EmailServiceTest에 code convention 적용하였습니다.

---
+++ src/test/java/com/yanolja_final/domain/user/controller/EmailControllerTest.java
@@ -0,0 +1,87 @@
+package com.yanolja_final.domain.user.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yanolja_final.domain.user.dto.request.EmailRequest;
+import com.yanolja_final.domain.user.service.EmailService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs(outputDir = "target/snippets")
+public class EmailControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EmailService emailService;
+
+    @BeforeEach
+    void setUp(RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+            .apply(documentationConfiguration(restDocumentation)).build();
+    }
+
+    @Test
+    void mailConfirm() throws Exception {
+        EmailRequest emailRequest = new EmailRequest();
+        emailRequest.setEmail("user@example.com");
+        given(emailService.sendVerificationEmail("user@example.com")).willReturn("200");
+
+        this.mockMvc.perform(post("/v1/email/confirmation")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(emailRequest)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.code", is(HttpStatus.OK.value())))
+            .andDo(document("v1/email/confirmation",
+                requestFields(
+                    fieldWithPath("email").type(JsonFieldType.STRING).description("이메일 주소")
+                ),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER).description("200")
+                )
+            ));
+    }
+
+    @Test
+    void verifyEmail() throws Exception {
+        given(emailService.verifyEmailCode("user@example.com", "123456")).willReturn(true);
+
+        this.mockMvc.perform(get("/v1/email/verify/123456")
+                .param("email", "user@example.com"))
+            .andExpect(status().isOk())
+            .andDo(document("v1/email/verify",
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER).description("200")
+                )
+            ));
+    }
+}
--- src/test/java/com/yanolja_final/domain/user/service/EmailServiceTest.java
+++ src/test/java/com/yanolja_final/domain/user/service/EmailServiceTest.java
@@ -2,8 +2,10 @@ package com.yanolja_final.domain.user.service;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import jakarta.mail.Session;
 import jakarta.mail.internet.MimeMessage;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.openMocks;
@@ -35,13 +38,10 @@ class EmailServiceTest {
         JavaMailSenderImpl mockMailSender = mock(JavaMailSenderImpl.class);
         MimeMessage mockMimeMessage = new MimeMessage((Session) null);
         when(mockMailSender.createMimeMessage()).thenReturn(mockMimeMessage);
-
         // Inject
         ReflectionTestUtils.setField(emailService, "javaMailSender", mockMailSender);
-
         // Act
         String code = emailService.sendVerificationEmail(email);
-
         // Assert
         assertNotNull(code);
         verify(mockMailSender).send(any(MimeMessage.class));
@@ -53,15 +53,12 @@ class EmailServiceTest {
         // Arrange
         String email = "test@example.com";
         String authCode = "12345678";
-
         // Inject
         Map<String, String> mockVerificationCodes = new HashMap<>();
         mockVerificationCodes.put(email, authCode);
         ReflectionTestUtils.setField(emailService, "verificationCodes", mockVerificationCodes);
-
         // Act
         boolean result = emailService.verifyEmailCode(email, authCode);
-
         // Assert
         assertTrue(result);
     }
@@ -72,10 +69,8 @@ class EmailServiceTest {
         // Arrange
         String email = "test@example.com";
         String code = "incorrect";
-
         // Act
         boolean result = emailService.verifyEmailCode(email, code);
-
         // Assert
         assertFalse(result);
     }
