fc237006c4c97c9c4e60ab3395cb9c854fbe4835
hym <ghrltjdtprbs@gmail.com>
2024-01-03 14:50:43 +0900

    [feat] SecurityConfig에 oauth2 로직 추가
    변경 사항:
     - SecurityConfig에 oauth2 로직 추가

--- src/main/java/com/yanolja_final/global/config/security/SecurityConfig.java
+++ src/main/java/com/yanolja_final/global/config/security/SecurityConfig.java
@@ -28,6 +28,8 @@ public class SecurityConfig {
     };
 
     private final JwtFilter jwtFilter;
+    private final PrincipalOauth2UserService principalOauth2UserService;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -72,6 +74,15 @@ public class SecurityConfig {
             jwtFilter,
             UsernamePasswordAuthenticationFilter.class
         );
+
+        http.oauth2Login(oauth2 -> oauth2
+            .userInfoEndpoint(
+                userInfoEndpoint -> userInfoEndpoint.userService(principalOauth2UserService))
+            .successHandler(oAuth2LoginSuccessHandler)
+        );
+        //사용자 프로필 정보를 가져오고 그 정보를 토대로 회원가입을 자동으로 진행
+        //정보가 추가적으로 필요하면 추가적으로 요구받아야함
+        //OAuth 완료가 되면 엑세스토큰 + 사용자 프로필 정보를 한번에 받음 로그인 성공시 oAuth2LoginSuccessHandler에서 처리해서 jwt토큰 response
 
         return http.build();
     }
