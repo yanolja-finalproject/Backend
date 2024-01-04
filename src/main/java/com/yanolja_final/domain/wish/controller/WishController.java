507d468329e435f1b9e8377ca30027b6d6991563
hym <ghrltjdtprbs@gmail.com>
2024-01-04 15:52:17 +0900

    [refactor] 페이지네이션 응답 유틸리티 추가 및 WishController에 적용
    변경 사항:
     - PaginationUtils 클래스 추가: 표준화된 페이지네이션 응답 구조 생성을 위한 유틸리티 메소드 구현
     - WishController 수정: PaginationUtils를 사용하여 getUserWishes 메소드의 응답 구조 개선

--- src/main/java/com/yanolja_final/domain/wish/controller/WishController.java
+++ src/main/java/com/yanolja_final/domain/wish/controller/WishController.java
@@ -5,16 +5,16 @@ import com.yanolja_final.domain.wish.dto.response.WishListResponse;
 import com.yanolja_final.domain.wish.entity.Wish;
 import com.yanolja_final.domain.wish.facade.WishFacade;
 import com.yanolja_final.global.config.argumentresolver.LoginedUserId;
+import com.yanolja_final.global.util.PaginationUtils;
 import com.yanolja_final.global.util.ResponseDTO;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -49,20 +48,10 @@ public class WishController {
     @GetMapping
     public ResponseEntity<ResponseDTO<Map<String, Object>>> getUserWishes(
         @LoginedUserId Long userId,
-        @RequestParam(defaultValue = "1") int page,
-        @RequestParam(defaultValue = "5") int pageSize) {
+        @PageableDefault(size = 5) Pageable pageable) {
 
-        Pageable pageable = PageRequest.of(page - 1, pageSize);
         Page<WishListResponse> wishesPage = wishFacade.getUserWishes(userId, pageable);
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("data", wishesPage.getContent());
-        response.put("page", Map.of(
-            "currentPage", wishesPage.getNumber() + 1,
-            "totalPage", wishesPage.getTotalPages(),
-            "currentElements", wishesPage.getNumberOfElements(),
-            "totalElements", wishesPage.getTotalElements()
-        ));
+        Map<String, Object> response = PaginationUtils.createPageResponse(wishesPage);
 
         return ResponseEntity.ok(ResponseDTO.okWithData(response));
     }
