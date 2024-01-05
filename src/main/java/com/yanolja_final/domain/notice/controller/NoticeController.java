043963e04ca86139a09e06949327077de990bd30
JeongByungJoo <cheng286@naver.com>
2024-01-06 00:40:57 +0900

    [feat] NoticeController에 공지사항 상세조회 Get 메서드 추가 : getSpecificNotice

--- src/main/java/com/yanolja_final/domain/notice/controller/NoticeController.java
+++ src/main/java/com/yanolja_final/domain/notice/controller/NoticeController.java
@@ -3,15 +3,17 @@ package com.yanolja_final.domain.notice.controller;
 
 import com.yanolja_final.domain.notice.dto.request.RegisterNoticeRequest;
 import com.yanolja_final.domain.notice.dto.response.NoticeListResponse;
-import com.yanolja_final.domain.notice.dto.response.RegisterNoticeResponse;
+import com.yanolja_final.domain.notice.dto.response.NoticeResponse;
 import com.yanolja_final.domain.notice.facade.NoticeFacade;
 import com.yanolja_final.global.util.ResponseDTO;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,10 +27,10 @@ public class NoticeController {
     private final NoticeFacade noticeFacade;
 
     @PostMapping
-    public ResponseEntity<ResponseDTO<RegisterNoticeResponse>> createNotice(
+    public ResponseEntity<ResponseDTO<NoticeResponse>> createNotice(
         @Valid @RequestBody RegisterNoticeRequest registerNoticeRequest
     ) {
-        ResponseDTO<RegisterNoticeResponse> response = noticeFacade.registerNotice(
+        ResponseDTO<NoticeResponse> response = noticeFacade.registerNotice(
             registerNoticeRequest);
 
         return ResponseEntity.status(HttpStatus.valueOf(response.getCode())).body(response);
@@ -41,5 +43,14 @@ public class NoticeController {
 
         return ResponseEntity.status(HttpStatus.valueOf(response.getCode())).body(response);
 
+    }
+
+    @GetMapping("/{noticeId}")
+    public ResponseEntity<ResponseDTO<NoticeResponse>> getSpecificNotice(
+        @PathVariable Long noticeId
+    ) {
+        ResponseDTO<NoticeResponse> response = noticeFacade.getSpecificNotice(noticeId);
+
+        return ResponseEntity.status(HttpStatusCode.valueOf(response.getCode())).body(response);
     }
 }
