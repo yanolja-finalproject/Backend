59ffa25af9dc672d9b75cad284dbc6f432a097c5
ChangHo <tony9911@naver.com>
2024-01-05 18:19:20 +0900

    [docs] 빌드시 poll 기본적으로 등록하는 sql 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,6 @@
 INSERT INTO authority(name) VALUES('ROLE_USER');
+
+INSERT INTO POLL (CREATED_AT, DELETED_AT, ID, UPDATED_AT, A_HASHTAG, A_NAME, B_HASHTAG, B_NAME, A_QUESTION, B_QUESTION, TITLE,A_COUNT,B_COUNT)
+VALUES
+    ('2024-01-03 12:00:00', NULL, 1, '2024-01-03 12:00:00', '#TagA1', 'NameA1', '#TagB1', 'NameB1', 'QuestionA', 'QuestionB', 'Sample Poll 1',0,0),
+    ('2024-01-03 12:30:00', NULL, 2, '2024-01-03 12:30:00', '#TagA2', 'NameA2', '#TagB2', 'NameB2', 'QuestionA'|| CHR(10) ||'문자열 슬라이드'|| CHR(10) ||'테스트용', 'QuestionY', 'Sample Poll 2',0,0)
