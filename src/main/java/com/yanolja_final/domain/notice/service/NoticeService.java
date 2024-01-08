e6f034b1d08825ddd847c43db98d6146a0f9e453
JeongByungJoo <cheng286@naver.com>
2024-01-08 13:43:54 +0900

    [feat] NoticeService_ 공지사항 상세조회 메서드_ getSpecificNotice 추가
    [refactor] 불필요한 줄바꿈 제거 /
    ~~Request / ~~Reponse의 instance 이름 request/ response로 가독성을 위해 변경

--- src/main/java/com/yanolja_final/domain/notice/service/NoticeService.java
+++ src/main/java/com/yanolja_final/domain/notice/service/NoticeService.java
@@ -4,6 +4,7 @@ import com.yanolja_final.domain.notice.dto.request.RegisterNoticeRequest;
 import com.yanolja_final.domain.notice.dto.response.NoticeListResponse;
 import com.yanolja_final.domain.notice.dto.response.NoticeResponse;
 import com.yanolja_final.domain.notice.entity.Notice;
+import com.yanolja_final.domain.notice.exception.NoticeNotFoundException;
 import com.yanolja_final.domain.notice.repository.NoticeRepository;
 import com.yanolja_final.global.util.ResponseDTO;
 import java.util.List;
@@ -16,21 +17,24 @@ public class NoticeService {
 
     private final NoticeRepository noticeRepository;
 
-    public ResponseDTO<NoticeResponse> registerNotice(
-        RegisterNoticeRequest registerNoticeRequest) {
-
-        Notice notice = registerNoticeRequest.toEntity();
+    public ResponseDTO<NoticeResponse> registerNotice(RegisterNoticeRequest request) {
+        Notice notice = request.toEntity();
         Notice newNotice = noticeRepository.save(notice);
-
         return ResponseDTO.okWithData(NoticeResponse.fromNotice(newNotice));
-
     }
+
 
     public ResponseDTO<List<NoticeListResponse>> getNoticeList() {
         List<Notice> notices = noticeRepository.findAll();
         List<NoticeListResponse> noticeListResponses = NoticeListResponse.fromNotices(notices);
-
         return ResponseDTO.okWithData(noticeListResponses);
     }
 
+
+    public ResponseDTO<NoticeResponse> getSpecificNotice(Long noticeId) {
+        Notice notice = noticeRepository.findById(noticeId)
+            .orElseThrow(() -> new NoticeNotFoundException());
+        NoticeResponse specificNoticeResponse = NoticeResponse.fromNotice(notice);
+        return ResponseDTO.okWithData(specificNoticeResponse);
+    }
 }
