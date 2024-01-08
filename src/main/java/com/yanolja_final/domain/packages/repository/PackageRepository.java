667891065a2f61869eb0a8ae690fbc85838be89a
hym <ghrltjdtprbs@gmail.com>
2024-01-09 08:30:26 +0900

    [feat] Package 정렬 로직 추가
    변경 사항 :
     - PackageService에 getPackagesByTheme 정렬 로직 추가
     - PackageRepository에 findByHashtagsContains 추가

--- src/main/java/com/yanolja_final/domain/packages/repository/PackageRepository.java
+++ src/main/java/com/yanolja_final/domain/packages/repository/PackageRepository.java
@@ -1,8 +1,11 @@
 package com.yanolja_final.domain.packages.repository;
 
+import com.yanolja_final.domain.packages.entity.Hashtag;
 import com.yanolja_final.domain.packages.entity.Package;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PackageRepository extends JpaRepository<Package, Long> {
-
+    Page<Package> findByHashtagsContains(Hashtag hashtag, Pageable pageable);
 }
--- src/main/java/com/yanolja_final/domain/packages/service/PackageService.java
+++ src/main/java/com/yanolja_final/domain/packages/service/PackageService.java
@@ -1,9 +1,14 @@
 package com.yanolja_final.domain.packages.service;
 
+import com.yanolja_final.domain.packages.entity.Hashtag;
 import com.yanolja_final.domain.packages.entity.Package;
 import com.yanolja_final.domain.packages.exception.PackageNotFoundException;
 import com.yanolja_final.domain.packages.repository.PackageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,5 +22,16 @@ public class PackageService {
     public Package findById(Long id) {
         return packageRepository.findById(id)
             .orElseThrow(() -> new PackageNotFoundException());
+    }
+
+    public Page<Package> getPackagesByTheme(Hashtag hashtag, String sortBy, Pageable pageable) {
+        Sort sort = Sort.by("departureTime").ascending();
+        if ("price_desc".equals(sortBy)) {
+            sort = Sort.by("price").descending();
+        } else if ("price_asc".equals(sortBy)) {
+            sort = Sort.by("price").ascending();
+        }
+        pageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), sort);
+        return packageRepository.findByHashtagsContains(hashtag, pageable);
     }
 }
