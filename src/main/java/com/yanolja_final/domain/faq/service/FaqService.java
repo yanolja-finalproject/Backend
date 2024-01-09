d672253b9b6c48a53d4afda484e9297616a8793b
JeongByungJoo <cheng286@naver.com>
2024-01-09 19:55:14 +0900

    [feat] FaqRepository Jpa 설정 및 FaqService_ 자주 하는 질문 등록 메서드 추가_ registerFaq

---
+++ src/main/java/com/yanolja_final/domain/faq/service/FaqService.java
@@ -0,0 +1,24 @@
+package com.yanolja_final.domain.faq.service;
+
+import com.yanolja_final.domain.faq.dto.request.RegisterFaqRequest;
+import com.yanolja_final.domain.faq.dto.response.FaqResponse;
+import com.yanolja_final.domain.faq.entity.Faq;
+import com.yanolja_final.domain.faq.facade.FaqFacade;
+import com.yanolja_final.domain.faq.repository.FaqRepository;
+import com.yanolja_final.global.util.ResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FaqService {
+
+    private final FaqRepository faqRepository;
+
+    public ResponseDTO<FaqResponse> registerFaq(RegisterFaqRequest request) {
+        Faq faq = request.toFaq();
+        Faq newFaq = faqRepository.save(faq);
+        FaqResponse response = FaqResponse.fromFaq(faq);
+        return ResponseDTO.okWithData(response);
+    }
+}
